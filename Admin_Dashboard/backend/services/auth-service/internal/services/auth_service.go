8906f2f28b76bd9fb70a0c3916678840bcc09cb3
DonShanilka <shanilka799@gmail.com>
2025-11-28 22:57:12 +0530

    refactor: consolidate user authentication methods into AuthService interface

--- Admin_Dashboard/backend/services/auth-service/internal/services/auth_service.go
+++ Admin_Dashboard/backend/services/auth-service/internal/services/auth_service.go
@@ -6,27 +6,51 @@ import (
     "golang.org/x/crypto/bcrypt"
     "github.com/golang-jwt/jwt/v5"
     "time"
+    "errors"
 )
 
-func RegisterUser(name, email, password string) error {
-    hashed, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-    user := models.User{Name: name, Email: email, Password: string(hashed)}
-    return repository.CreateUser(user)
+type AuthService interface {
+    Register(user *models.User) error
+    Login(email, password string) (string, error)
 }
 
-func LoginUser(email, password string) (string, error) {
-    user, err := repository.FindUserByEmail(email)
-    if err != nil { return "", err }
+type authService struct {
+    UserRepo repository.UserRepository
+    JWTSecret string
+}
 
-    if bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)) != nil {
-        return "", err
+func NewAuthService(repo repository.UserRepository, jwtSecret string) AuthService {
+    return &authService{
+        UserRepo: repo,
+        JWTSecret: jwtSecret,
+    }
+}
+
+func (s *authService) Register(user *models.User) error {
+    hash, _:= bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+    user.Password = string(hash)
+    return s.UserRepo.CreateUser(user)
+}
+
+func (s *authService) Login(email, password string) (*models.TokenResponse, error) {
+    user, err := s.UserRepo.FindUserByEmail(email)
+    if err != nil {
+        return nil, errors.New("Email not found")
+    }
+
+    err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+    if err != nil {
+        return nil, errors.New("Invalid password")
     }
 
     token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-        "id": user.ID,
-        "email": user.Email,
-        "exp": time.Now().Add(time.Hour * 24).Unix(),
+        "userID": user.ID,
+        "email":  user.Email,
+        "exp":    time.Now().Add(time.Hour * 72).Unix(),
     })
 
-    return token.SignedString([]byte("MY_SECRET_KEY"))
+    tokenString, _ := token.SignedString([]byte(s.JWTSecret))
+    
+
+    return &models.TokenResponse{Token: tokenString}, nil
 }
