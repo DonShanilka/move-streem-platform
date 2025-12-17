1bedbd25e59331ff2b2b89403577c9349bf7a2c3
DonShanilka <shanilka799@gmail.com>
2025-12-17 22:40:01 +0530

    update series service

--- backend/services/tvSeries-service/internal/Service/tvSeries_service.go
+++ backend/services/tvSeries-service/internal/Service/tvSeries_service.go
@@ -1,44 +1,18 @@
 package Service
 
 import (
-	"errors"
-
 	"github.com/DonShanilka/tvSeries-service/internal/Models"
 	"github.com/DonShanilka/tvSeries-service/internal/Repository"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type TvSeriesService struct {
+type TvSerriesService struct {
 	Repo *Repository.TvSeriesRepository
 }
 
-func NewTvSeriesService(
-	repo *Repository.TvSeriesRepository,
-) *TvSeriesService {
-	return &TvSeriesService{Repo: repo}
+func NewTvSerriesService(repo *Repository.TvSeriesRepository) *TvSerriesService {
+	return &TvSerriesService{Repo: repo}
 }
 
-// ---------------- CREATE SERIES ----------------
-func (s *TvSeriesService) CreateSeries(
-	series *Models.Series,
-) (primitive.ObjectID, error) {
-
-	if series.Title == "" {
-		return primitive.NilObjectID, errors.New("title is required")
-	}
-
-	return s.Repo.CreateSeries(series)
-}
-
-// ---------------- ADD SEASON ----------------
-func (s *TvSeriesService) AddSeason(
-	seriesID primitive.ObjectID,
-	season Models.Season,
-) error {
-
-	if season.SeasonNumber <= 0 {
-		return errors.New("invalid season number")
-	}
-
-	return s.Repo.AddSeasonToSeries(seriesID, season)
+func (s *TvSerriesService) CreateTvSeries(tvSerries *Models.Series) error {
+	return s.Repo.Create(tvSerries)
 }
