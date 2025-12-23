f2fc1435d634e69066c4923f429104229306d9de
DonShanilka <shanilka799@gmail.com>
2025-12-23 13:51:48 +0530

    update tvSeries_service.go

--- backend/services/tvSeries-service/internal/Service/tvSeries_service.go
+++ backend/services/tvSeries-service/internal/Service/tvSeries_service.go
@@ -1,8 +1,11 @@
 package Service
 
 import (
+	"errors"
+
 	"github.com/DonShanilka/tvSeries-service/internal/Models"
 	"github.com/DonShanilka/tvSeries-service/internal/Repository"
+	"gorm.io/gorm"
 )
 
 type TvSerriesService struct {
@@ -13,6 +16,47 @@ func NewTvSerriesService(repo *Repository.TvSeriesRepository) *TvSerriesService 
 	return &TvSerriesService{Repo: repo}
 }
 
-func (s *TvSerriesService) CreateTvSeries(tvSerries *Models.Series) error {
-	return s.Repo.Create(tvSerries)
+func (s *TvSerriesService) CreateTvSeries(tvSeries *Models.Series) error {
+	return s.Repo.Create(tvSeries)
+}
+
+func (s *TvSerriesService) GetAllTvSeries() ([]Models.Series, error) {
+	return s.Repo.GetAll()
+}
+
+func (s *TvSerriesService) GetTvSeriesByID(id uint) (*Models.Series, error) {
+	series, err := s.Repo.GetByID(id)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, errors.New("tv series not found")
+		}
+		return nil, err
+	}
+	return series, nil
+}
+
+func (s *TvSerriesService) UpdateTvSeries(id uint, data *Models.Series) error {
+	// Ensure series exists before update
+	_, err := s.Repo.GetByID(id)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return errors.New("tv series not found")
+		}
+		return err
+	}
+
+	return s.Repo.Update(id, data)
+}
+
+func (s *TvSerriesService) DeleteTvSeries(id uint) error {
+	// Ensure series exists before delete
+	_, err := s.Repo.GetByID(id)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return errors.New("tv series not found")
+		}
+		return err
+	}
+
+	return s.Repo.Delete(id)
 }
